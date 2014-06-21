81ece42226ef5ee313eece2533c9a99ebdc9517e
nightshadow404 <keiranlivers@yahoo.co.uk>
2014-06-21 23:09:58 +0100

    0.1a

---
+++ src/main/java/nightshadow404/Cannoneer/Cannoneer.java
@@ -0,0 +1,35 @@
+package nightshadow404.Cannoneer;
+
+
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+
+
+@Mod(modid="N404Cannoneer", name="nightshadow404's Cannoneer", version="1.7.2-0.1a")
+
+
+public class Cannoneer {
+
+    @Mod.Instance("Cannoneer")
+    public static Cannoneer instance;
+
+    @Mod.EventHandler
+    public void preInit(FMLPreInitializationEvent pre)
+    {
+
+    }
+
+    @Mod.EventHandler
+    public void init(FMLInitializationEvent init)
+    {
+
+    }
+
+    @Mod.EventHandler
+    public void postInit(FMLPostInitializationEvent post)
+    {
+
+    }
+}
